c6a938694cb9484e10f50ac696b9f26b2566edcd
aaroncui <aaroncui@tencent.com>
2019-11-08 20:06:41 +0800

    test: add mounting test

--- tests/comment.test.tsx
+++ tests/comment.test.tsx
@@ -1,8 +1,13 @@
-import * as React from 'react';
+import * as _ from 'lodash';
 import * as ReactDOM from 'react-dom';
 import { Comment } from '../src/index';
+import * as React from 'react';
 
 describe('component: comment', () => {
+  afterEach(() => {
+    document.getElementsByTagName('html')[0].innerHTML = '';
+  });
+
   it('# should mount', () => {
     const ELEMENT_ID = 'disqus-comment';
     const el = document.createElement('div');
@@ -10,6 +15,34 @@ describe('component: comment', () => {
     document.body.append(el);
     ReactDOM.render(<Comment title="" identifier="" url="" shortname="" />, document.getElementById(ELEMENT_ID));
   });
-  it('# should create disqus comment div when not exists', () => {});
-  it('# should call `reset` when exist', () => {});
+
+  it('# should create disqus comment div when not exists', async (done) => {
+    const DISQUS_COMMENT_ELEMENT_ID = 'dsq-embed-scr';
+
+    // before, ensure is not exists
+    expect(document.getElementById(DISQUS_COMMENT_ELEMENT_ID)).toBeNull();
+
+    // mount
+    const ELEMENT_ID = 'disqus-comment';
+    const MOCK_DISQUS_SHORTNAME = 'foo';
+    const MOCK_DISQUS_IDENTIFIER = 'foo-id';
+    const MOCK_DISQUS_URL = 'foo-url';
+    const el = document.createElement('div');
+    el.setAttribute('id', ELEMENT_ID);
+    document.body.append(el);
+    ReactDOM.render(
+      <Comment title="" identifier={MOCK_DISQUS_IDENTIFIER} url={MOCK_DISQUS_URL} shortname={MOCK_DISQUS_SHORTNAME} />,
+      document.getElementById(ELEMENT_ID)
+    );
+
+    _.delay(() => {
+      expect(document.querySelector('script[src="https://foo.disqus.com/embed.js"]')).not.toBeNull();
+      expect(window['disqus_config']).toBeDefined();
+      done();
+    }, 3000);
+  });
+
+  it('# should call `reset` when exist', () => {
+    // TODO: suppose to load with puppeteer instead of DOM
+  });
 });
