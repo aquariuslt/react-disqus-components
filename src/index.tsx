31cce3a184dc253d524eb00f2d7cd4a20fffea01
aquariuslt <superaquariuslt@gmail.com>
2019-11-10 01:56:30 +0800

    feat: add `cleanInstance` as unmount effect

--- src/index.tsx
+++ src/index.tsx
@@ -16,10 +16,11 @@ const EMPTY_DISQUS_CONFIG = {
 const DISQUS_INSTANCE = 'DISQUS';
 const DISQUS_CONFIG = 'disqus_config';
 const DISQUS_SHORTNAME = 'disqus_shortname';
+const DISQUS_THREAD = 'disqus_thread';
+const DISQUS_COMMENT_ELEMENT_ID = 'dsq-embed-scr';
 
 export const Comment: React.FC<DisqusCommentProps> = (props) => {
   const disqusConfig = props || EMPTY_DISQUS_CONFIG;
-  const disqusCommentElementId = 'dsq-embed-scr';
 
   const insertScript = (src, id, parentElement) => {
     const script = window.document.createElement('script');
@@ -28,6 +29,13 @@ export const Comment: React.FC<DisqusCommentProps> = (props) => {
     script.id = id;
     parentElement.appendChild(script);
     return script;
+  };
+
+  const removeScript = (id, parentElement) => {
+    const script = window.document.getElementById(id);
+    if (script) {
+      parentElement.removeChild(script);
+    }
   };
 
   const getDisqusConfig = () => {
@@ -39,10 +47,10 @@ export const Comment: React.FC<DisqusCommentProps> = (props) => {
   };
 
   const loadInstance = () => {
-    if (!document.getElementById(disqusCommentElementId) && disqusConfig.shortname) {
+    if (!document.getElementById(DISQUS_COMMENT_ELEMENT_ID) && disqusConfig.shortname) {
       window[DISQUS_CONFIG] = getDisqusConfig();
       window[DISQUS_SHORTNAME] = disqusConfig.shortname;
-      insertScript(`https://${disqusConfig.shortname}.disqus.com/embed.js`, disqusCommentElementId, document.body);
+      insertScript(`https://${disqusConfig.shortname}.disqus.com/embed.js`, DISQUS_COMMENT_ELEMENT_ID, document.body);
     } else {
       if (window[DISQUS_INSTANCE]) {
         window[DISQUS_INSTANCE].reset({
@@ -53,9 +61,24 @@ export const Comment: React.FC<DisqusCommentProps> = (props) => {
     }
   };
 
+  const cleanInstance = () => {
+    if (window[DISQUS_INSTANCE] && document.getElementById(DISQUS_COMMENT_ELEMENT_ID)) {
+      window[DISQUS_INSTANCE].reset();
+      window[DISQUS_INSTANCE] = undefined;
+      removeScript(DISQUS_COMMENT_ELEMENT_ID, document.body);
+      const disqusThread = document.getElementById(DISQUS_THREAD);
+      if (disqusThread) {
+        while (disqusThread.hasChildNodes() && disqusThread.firstChild) {
+          disqusThread.removeChild(disqusThread.firstChild);
+        }
+      }
+    }
+  };
+
   useEffect(() => {
     loadInstance();
+    return cleanInstance;
   }, [props]);
 
-  return <div id="disqus_thread" />;
+  return <div id={DISQUS_THREAD} />;
 };
