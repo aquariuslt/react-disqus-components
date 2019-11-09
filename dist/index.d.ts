b7816cc4e984519ee8f5298d0129446111610c56
aquariuslt <superaquariuslt@gmail.com>
2019-11-10 02:04:15 +0800

    chore: fix release assets config

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-export interface DisqusCommentProps {
-    title: string;
-    identifier: string;
-    url: string;
-    shortname: string;
-}
-export declare const Comment: React.FC<DisqusCommentProps>;
